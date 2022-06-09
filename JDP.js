1346d5aeccec16ba01de338e560cc64744e2e1a1
Jams <Jamsjam971@hotmail.com>
2022-06-09 11:41:32 -0400

    Fix de boucle infini// debug du clavier en HTML// ancienne commande en commentaire, refaire le JS

--- JDP.js
+++ JDP.js
@@ -21,39 +21,48 @@ window.onload = function(){
     var monTableau = [];
     var monTableauUser = [];
     var i=0;
-    var usedLettre 
-    var f = 0
-    var motChoisis
+    var usedLettre;
+    var f = 0;
+    var motChoisis;
     document.querySelector('#start').onclick = function() {
 
-        document.querySelector('#indication').innerHTML = 
-        '<p>Choisir le mot de depart</p>'+ 
-        '<input id="votreMot" type="text" placeholder="noter votre mot ici"></input>'+
-        '<button id="submit" type="button"> envoyer</button>'
-        document.querySelector('#start').style.display = 'none'
+        document.querySelector('#indication').style.display = 'none';
+        // '<p>Choisir le mot de depart</p>'+ 
+        // '<input id="votreMot" type="text" placeholder="noter votre mot ici"></input>'+
+        // '<button id="submit" type="button"> envoyer</button>';
+        document.querySelector('#start').style.display = 'none';
+        document.querySelector('#choixMot').style.display = 'block';
+        
+    };
+    document.querySelector('#submit').onclick = function() {
+        
+        motChoisis = document.querySelector('#votreMot').value.toLowerCase();
+        
+        document.querySelector('#motChoisis').innerText = motChoisis
+        
+        
+        while (i < motChoisis.length){
+            monTableau.push(motChoisis.slice(i , i+1));
+            monTableauUser.splice(i,0,"_");
+            i++;
+        };
+        
+        console.log(monTableau)
+        console.log(monTableauUser)
+        document.querySelector('#motUser').innerText = monTableauUser
+        document.querySelector('#clavier').style.display = 'block';
+        document.querySelector('#indication').innerHTML = '<p>Cliquez une lettre</p>'
+        document.querySelector('#choixMot').style.display = 'none';
+    };
+        
 
-            document.querySelector('#submit').onclick = function() {
 
-                motChoisis = document.querySelector('#votreMot').value.toLowerCase();
-                
-                document.querySelector('#motChoisis').innerText = motChoisis
-                
 
-                while (i < motChoisis.length){
-                    monTableau.push(motChoisis.slice(i , i+1));
-                    monTableauUser.splice(i,0,"_");
-                    i++
-                };
 
-                console.log(monTableau)
-                console.log(monTableauUser)
-                document.querySelector('#motUser').innerText = monTableauUser
-                document.querySelector('#clavier').style.display = 'block';
-                document.querySelector('#indication').innerHTML = '<p>Cliquez une lettre</p>'
-                
-            };
-    }
 
+
+
+    
     var a = monTableauUser.join()
     var b = monTableau.join()
     
@@ -63,161 +72,162 @@ window.onload = function(){
 
 
 
-    while(f<6){
-        //******************************** Etape 2 : Verifier la lettre
+    // while(f<6){
+    //     //******************************** Etape 2 : Verifier la lettre
 
 
         //Fonction par laquelle la valeur du clavier passera
         function choixLettre(lettre) {
             var lettreChoisis = lettre
-            console.log('silver')
+            console.log(lettreChoisis)
+        }
 
-            document.querySelector('#a').onclick = function() {
+            document.querySelector('#a').onclick = () => {
                 choixLettre('a');
-                this.style.backgroundColor = 'silver';
+                // this.style.backgroundColor = 'silver';
                 
             }
-            document.querySelector('#b').onclick = function() {
+            document.querySelector('#b').onclick = () => {
                 choixLettre('b');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#c').onclick = function() {
+            document.querySelector('#c').onclick = () => {
                 choixLettre('c');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#d').onclick = function() {
+            document.querySelector('#d').onclick = () => {
                 choixLettre('d');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#e').onclick = function() {
+            document.querySelector('#e').onclick = () => {
                 choixLettre('e');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#f').onclick = function() {
+            document.querySelector('#f').onclick = () => {
                 choixLettre('f');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#g').onclick = function() {
+            document.querySelector('#g').onclick = () => {
                 choixLettre('g');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#h').onclick = function() {
+            document.querySelector('#h').onclick = () => {
                 choixLettre('h');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#i').onclick = function() {
+            document.querySelector('#i').onclick = () => {
                 choixLettre('i');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#j').onclick = function() {
+            document.querySelector('#j').onclick = () => {
                 choixLettre('j');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#k').onclick = function() {
+            document.querySelector('#k').onclick = () => {
                 choixLettre('k');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#l').onclick = function() {
+            document.querySelector('#l').onclick = () => {
                 choixLettre('l');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#m').onclick = function() {
+            document.querySelector('#m').onclick = () => {
                 choixLettre('m');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#n').onclick = function() {
+            document.querySelector('#n').onclick = () => {
                 choixLettre('n');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#o').onclick = function() {
+            document.querySelector('#o').onclick = () => {
                 choixLettre('o');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#p').onclick = function() {
+            document.querySelector('#p').onclick = () => {
                 choixLettre('p');
-                this.style.backgroundColor = 'silver';
+                // this.style.backgroundColor = 'silver';
             }
-            document.querySelector('#q').onclick = function() {
+            document.querySelector('#q').onclick = () => {
                 choixLettre('q');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#r').onclick = function() {
+            document.querySelector('#r').onclick = () => {
                 choixLettre('r');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#s').onclick = function() {
+            document.querySelector('#s').onclick = () => {
                 choixLettre('s');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#t').onclick = function() {
+            document.querySelector('#t').onclick = () => {
                 choixLettre('t');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#u').onclick = function() {
+            document.querySelector('#u').onclick = () => {
                 choixLettre('u');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#v').onclick = function() {
+            document.querySelector('#v').onclick = () => {
                 choixLettre('v');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#w').onclick = function() {
+            document.querySelector('#w').onclick = () => {
                 choixLettre('w');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#x').onclick = function() {
+            document.querySelector('#x').onclick = () => {
                 choixLettre('x');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#y').onclick = function() {
+            document.querySelector('#y').onclick = () => {
                 choixLettre('y');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
-            document.querySelector('#z').onclick = function() {
+            document.querySelector('#z').onclick = () => {
                 choixLettre('z');
-                this.style.backgroundColor = 'silver'
+                // this.style.backgroundColor = 'silver'
             }
 
 
     
-            if (usedLettre.indexOf(lettreChoisis) != -1){
-                document.querySelector('#indication').innerText = "Tu as deja utiliser cette lettre choisis en une autre";
-            //     document.querySelector('#'+lettre).style.backgroundColor = 'red'
-                //         console.log(monTableau);
-                //         console.log(monTableauUser);
-                //         console.log(f);
-            }
-            else{
-                usedLettre = usedLettre + lettre
-                if (motChoisis.indexOf(lettreChoisis) = -1){
-                    document.querySelector('#indication').innerHTML = '<p>La lettre n\'est pas la bonne</p><p>Choisissez une autre lettre</p>';
+    //         // if (usedLettre.indexOf(lettreChoisis) != -1){
+    //         //     document.querySelector('#indication').innerText = "Tu as deja utiliser cette lettre choisis en une autre";
+    //         // //     document.querySelector('#'+lettre).style.backgroundColor = 'red'
+    //         //     //         console.log(monTableau);
+    //         //     //         console.log(monTableauUser);
+    //         //     //         console.log(f);
+    //         // }
+    //         // else{
+    //         //     usedLettre = usedLettre + lettre
+    //         //     if (motChoisis.indexOf(lettreChoisis) = -1){
+    //         //         document.querySelector('#indication').innerHTML = '<p>La lettre n\'est pas la bonne</p><p>Choisissez une autre lettre</p>';
                     
-                    f++
+    //         //         f++
 
 
-                }
-                else{
-                        for (var idx=0; idx != -1;idx++){
+    //         //     }
+    //         //     else{
+    //         //             for (var idx=0; idx != -1;idx++){
                         
-                        idx = (monTableau.indexOf(lettre));
-                        monTableauUser.splice(idx,1,lettre);  //teste si le mot est present plusieurs fois
-                        idx = monTableau.indexOf(lettre, idx+1);
-                        document.querySelector('#motUser').innerText = monTableauUser;
-                        document.querySelector('#indication').innerHTML = '<p>Choisissez une autre lettre</p>'
-                        idx++
-                        }
-                }
-            }
+    //         //             idx = (monTableau.indexOf(lettre));
+    //         //             monTableauUser.splice(idx,1,lettre);  //teste si le mot est present plusieurs fois
+    //         //             idx = monTableau.indexOf(lettre, idx+1);
+    //         //             document.querySelector('#motUser').innerText = monTableauUser;
+    //         //             document.querySelector('#indication').innerHTML = '<p>Choisissez une autre lettre</p>'
+    //         //             idx++
+    //         //             }
+    //         //     }
+    //         // }
 
             
-        }
+    //     }
         //******************************** Etape 3 condition victoire/defaite
-        if ( a = b ) {
-            f=10
-            document.querySelector('#indication').innerHTML = '<h2>VOUS AVEZ GAGNER</h2>'
-        }
+        // if ( a = b ) {
+        //     f=10
+        //     document.querySelector('#indication').innerHTML = '<h2>VOUS AVEZ GAGNER</h2>'
+        // }
 
-    }
+    // }
 
 
 
